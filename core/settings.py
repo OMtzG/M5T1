f93304310ced03b510537240cb62e4170ddc5e21
Oscar Martinez <mtz.galan.oscar@gmail.com>
2026-08-26 18:20:54 +0200

    corregido warning de Django configurando DEFAULT_AUTO_FIELD a BigAutoField

--- core/settings.py
+++ core/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_URL = 'Peliculas:login'
 LOGIN_REDIRECT_URL = 'Peliculas:home'
 LOGOUT_REDIRECT_URL = 'Peliculas:home'
